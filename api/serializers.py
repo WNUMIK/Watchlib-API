98f262694eb48d6aab2bf5e2d45161bce4747bbe
wnumik <k1wsatz@wnumiks-MacBook-Pro.local>
2022-05-22 14:48:27 +0200

    create func in serializer and post view in APIview

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 
+from movie.models import Movie
+
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
+
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,14 +7,21 @@ from movie.models import Movie
 
 
 class MovieList(APIView):
-    def get(self, request, format=None):
+    def get(self, request):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class MovieDetail(APIView):
-    def get(self, request, pk, format=None):
+    def get(self, request, pk):
         movie = Movie.objects.get(pk=pk)
         serializer = MovieSerializer(movie)
         return Response(serializer.data)

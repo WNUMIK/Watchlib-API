f1a6c9ff3ad6f6b3aeb2bd9bcc3627e9e1e2f676
wnumik <k1wsatz@wnumiks-MacBook-Pro.local>
2022-05-22 14:58:44 +0200

    methods for APIView

--- api/views.py
+++ api/views.py
@@ -25,3 +25,16 @@ class MovieDetail(APIView):
         movie = Movie.objects.get(pk=pk)
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response()

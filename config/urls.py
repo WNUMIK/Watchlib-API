2e4fd736e2e3c549334f961c66f6e4e6e4fae051
wnumik <majkel216@gmail.com>
2022-05-24 13:59:23 +0200

    test cases

--- config/urls.py
+++ config/urls.py
@@ -26,9 +26,9 @@ schema_view = get_schema_view(
    openapi.Info(
       title="Watchlib API",
       default_version='v1',
-      description="Test description",
+      description="Watch library with DRF",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
+      contact=openapi.Contact(email="truecodelove@gmail.com"),
       license=openapi.License(name="Test License"),
    ),
    public=True,
--- watch/tests.py
+++ watch/tests.py
@@ -15,6 +15,8 @@ class StreamPlatformTestCaseAdmin(APITestCase):
         self.user = User.objects.create_superuser(username='admin', password='TestAdmin@123')
         self.token = Token.objects.get(user__username=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.stream = models.StreamPlatform.objects.create(name="TestPlatform", about="Tests",
+                                                           website="https://www.tests.com")
 
     def test_post_create(self):
         data = {
@@ -24,6 +26,10 @@ class StreamPlatformTestCaseAdmin(APITestCase):
         }
         response = self.client.post(reverse('stream-platform-list'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_delete_list(self):
+        response = self.client.delete(reverse('stream-platform-detail', args=(self.stream.id,)))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
 
 class StreamPlatformTestCaseUser(APITestCase):
@@ -79,7 +85,8 @@ class WatchListTestCase(APITestCase):
 
         self.stream = models.StreamPlatform.objects.create(name="TestPlatform", about="Tests",
                                                            website="https://www.tests.com")
-        self.watchlist = models.WatchList.objects.create(platform=self.stream, title="Test Title", storyline="Test Storyline", active=True)
+        self.watchlist = models.WatchList.objects.create(platform=self.stream, title="Test Title",
+                                                         storyline="Test Storyline", active=True)
 
     def test_post_create(self):
         data = {
@@ -97,4 +104,77 @@ class WatchListTestCase(APITestCase):
 
     def test_get_detail(self):
         response = self.client.get(reverse('watch-detail', args=(self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.WatchList.objects.count(), 1)
+        self.assertEqual(models.WatchList.objects.get().title, 'Test Title')
+
+class ReviewTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="Password@123")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(name="Netflix",
+                                                           about="#1 Platform", website="https://www.netflix.com")
+        self.watchlist = models.WatchList.objects.create(platform=self.stream, title="Example Movie",
+                                                         storyline="Example Movie", active=True)
+        self.watchlist2 = models.WatchList.objects.create(platform=self.stream, title="Example Movie",
+                                                          storyline="Example Movie", active=True)
+        self.review = models.Review.objects.create(review_user=self.user, rating=5, description="Great Movie",
+                                                   watchlist=self.watchlist2, active=True)
+
+    def test_review_create(self):
+        data = {
+            "review_user": self.user,
+            "rating": 5,
+            "description": "Great Movie!",
+            "watchlist": self.watchlist,
+            "active": True
+        }
+
+        response = self.client.post(reverse('review-create', args=(self.watchlist.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(models.Review.objects.count(), 2)
+
+        response = self.client.post(reverse('review-create', args=(self.watchlist.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_review_create_unauth(self):
+        data = {
+            "review_user": self.user,
+            "rating": 5,
+            "description": "Great Movie!",
+            "watchlist": self.watchlist,
+            "active": True
+        }
+
+        self.client.force_authenticate(user=None)
+        response = self.client.post(reverse('review-create', args=(self.watchlist.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_review_update(self):
+        data = {
+            "review_user": self.user,
+            "rating": 4,
+            "description": "Great Movie! - Updated",
+            "watchlist": self.watchlist,
+            "active": False
+        }
+        response = self.client.put(reverse('review-detail', args=(self.review.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_review_list(self):
+        response = self.client.get(reverse('review-list', args=(self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_review_ind(self):
+        response = self.client.get(reverse('review-detail', args=(self.review.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_review_ind_delete(self):
+        response = self.client.delete(reverse('review-detail', args=(self.review.id,)))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_review_user(self):
+        response = self.client.get('/watch/reviews/?username' + self.user.username)
         self.assertEqual(response.status_code, status.HTTP_200_OK)

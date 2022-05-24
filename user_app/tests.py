18885ef800e58696055578a1d20815e669ea7b65
wnumik <majkel216@gmail.com>
2022-05-24 12:24:57 +0200

    tests

--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+
+class RegisterTestCase(APITestCase):
+
+    def test_post_register(self):
+        data = {
+            "username": "testcase",
+            "email": "test@case.com",
+            "password": "TestCase@123",
+            "password2": "TestCase@123"
+        }
+        response = self.client.post(reverse('register'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class LoginLogoutTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username="example", password="Example@123")
+
+    def test_post_login(self):
+        data = {
+            "username": "example",
+            "password": "Example@123"
+        }
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_logout(self):
+        self.token = Token.objects.get(user__username="example")
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- watch/tests.py
+++ watch/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+from watch.api import serializers
+from watch import models
+
+
+class StreamPlatformTestCaseAdmin(APITestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create_superuser(username='admin', password='TestAdmin@123')
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_post_create(self):
+        data = {
+            "name": "Netshit",
+            "about": "Sh111y platform",
+            "website": "https://wwww.netshit.com"
+        }
+        response = self.client.post(reverse('stream-platform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class StreamPlatformTestCaseUser(APITestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username='user', password='TestUser@123')
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(name="TestPlatform", about="Tests",
+                                                           website="https://www.tests.com")
+
+    def test_post_create(self):
+        data = {
+            "name": "Netshit",
+            "about": "Sh111y platform",
+            "website": "https://wwww.netshit.com"
+        }
+        response = self.client.post(reverse('stream-platform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_list(self):
+        response = self.client.get(reverse('stream-platform-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_detail(self):
+        response = self.client.get(reverse('stream-platform-detail', args=(self.stream.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
